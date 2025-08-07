1150034085a0072af421ded1ee4e0af6050140f5
iosi poli <109917623+sesese1234@users.noreply.github.com>
2025-08-08 02:33:07 +0300

    Update build.gradle.kts
    chage the version name to 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 22
         targetSdk = 34
         versionCode = 1
-        versionName = "0.4.0"
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
